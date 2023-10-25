3171d560bb7c9e47415a14879e0637af2d35bdf9
zeze <thwjd2717@gmail.com>
2023-10-25 13:03:30 +0900

    add: 서블릿으로 여러 컨텐츠 타입 현태로 http 메세지 주고 받기 구현

---
+++ servlet/src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -0,0 +1,30 @@
+package hello.servlet.basic.response;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "responseHeaderServlet", urlPatterns = "/response-header")
+public class ResponseHeaderServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // status-line
+        resp.setStatus(HttpServletResponse.SC_OK); // 상태코드 200
+
+        // resp-headers
+        resp.setHeader("Content-Type", "test/plain;charset=utf-8");
+        resp.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        resp.setHeader("Pragma", "no-cache");
+        resp.setHeader("my-header", "hello");
+
+        PrintWriter writer = resp.getWriter();
+        writer.println("ok");
+    }
+}
