13160f679ba8892b5ed1af2675ac1c0ea396a8f9
zeze <thwjd2717@gmail.com>
2023-11-14 13:43:21 +0900

    feat: ControllerV4에서 ModelView를 직접 생성해서 반환하지 않도록 편리한 인터페이스 제공

--- servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
     private String viewPath;
@@ -13,8 +14,15 @@ public class MyView {
         this.viewPath = viewPath;
     }
 
-    public void render(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        modelToRequestAttribute(model, request);
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response); // JSP 렌더링
+    }
+
+    // ModelView 객체 생성 시는 viewpath 와 map<str, obj> model이 필요함
+    public void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest req) throws ServletException, IOException {
+        // model 안에 들어 있는 값을 다 꺼내서, req의 req에 값을 다 담음
+        model.forEach((key, value) -> req.setAttribute(key, value));
     }
 }
