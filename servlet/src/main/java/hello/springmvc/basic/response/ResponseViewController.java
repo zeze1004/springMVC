294441fe0db064d06ec674f89d51a96ff570ca30
zeze <thwjd2717@gmail.com>
2023-11-23 10:50:06 +0900

    chore: view controller 샘플 코드 작성

---
+++ servlet/src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,15 @@
+package hello.springmvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseViewV1() {
+        ModelAndView mav = new ModelAndView("response/hello")
+            .addObject("data", "hello!");
+        return mav;
+    }
+}
